a3ea0c4b5f16549e3dc2b65991a467b5fe8d51f3
Matheo <kuktoskymatheo@gmail.com>
2020-12-18 03:28:59 -0300

    MySql and React-App

--- back/dbScript.sql
+++ back/dbScript.sql
@@ -1,6 +1,8 @@
 drop database if exists sql_server;
 create database sql_server;
 use sql_server;
+ALTER USER 'admin' IDENTIFIED WITH mysql_native_password BY 'admin';
+flush privileges;
 
 create table stats (
     stat_id int NOT NULL AUTO_INCREMENT,
@@ -10,3 +12,5 @@ create table stats (
     score int NOT NULL,
     PRIMARY KEY (stat_id)    
 );
+select * from stats;
+insert into stats (nickname, p_image, creation_date, score) values ("toninho", "img", "1970-01-01 00:00:01", 100);
